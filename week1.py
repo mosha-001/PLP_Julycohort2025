862af5825cf1011c76e24db606e92a65f5ddf210
alfamvrck <alfamvrck@gmail.com>
2025-08-19 10:36:40 +0300

    Added two python assingment files.

---
+++ week1.py
@@ -0,0 +1,16 @@
+def calc(num1, num2, operator):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Error: Invalid operator"
+    
+print(calc(5, 3, "+"))
